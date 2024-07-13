7b9c8882ec22d81cbfa37fee05fdc0035fc355b0
ManeshLokare <maneshlokare123@gmail.com>
2024-07-13 20:12:55 +0530

    new file added

---
+++ src/ex_130724/Lab001.java
@@ -0,0 +1,23 @@
+package ex_130724;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        // data type
+        // primitve data types
+     byte b=10;
+     int age=25;
+     short s=200;
+     long phone_number=1234567890l;
+     char ch= 'm';
+     boolean bo= true;
+     float f=20.10f;
+     double d= 118.542;
+
+     System.out.println(b);
+
+     // non-primitive data types
+        String name ="Manesh";
+        System.out.println(name);
+
+    }
+}
---
+++ src/ex_130724/Lab002.java
@@ -0,0 +1,16 @@
+package ex_130724;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        // concandition
+
+        String name ="Manesh";
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a+b);
+        System.out.println(a+b+name);
+        System.out.println(name+a+b);
+        System.out.println(a+name+b);
+    }
+}
