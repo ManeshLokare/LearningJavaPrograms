df743ee1725fe35b94611f95272d7e1ecaf2c914
ManeshLokare <maneshlokare123@gmail.com>
2024-07-14 19:12:02 +0530

    new file added

---
+++ src/ex_140724/Lab002.java
@@ -0,0 +1,17 @@
+package ex_140724;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        // data types
+        boolean bo= true;       // byte=0, Bits=0
+        byte b=10;              // byte=1, Bits=8
+        char ch= 'm';           //byte=2, Bits=16
+        short s=200;            //byte=2, Bits=16
+        int age=25;             //byte=4, Bits=32
+        long phone_number=1234567890l; //byte=8, Bits=64
+        float f=20.10f;            //byte=4, Bits=32
+        double d= 118.542;         //byte=8, Bits=64
+        // any reference type
+        String name="manesh";    // byte=8, Bits=64
+    }
+}
