cda73a93fba9b77880290546d4be052fcadc5fe5
ManeshLokare <maneshlokare123@gmail.com>
2024-07-21 12:34:21 +0530

    new file added

---
+++ src/ex_210724/Lab001.java
@@ -0,0 +1,15 @@
+package ex_210724;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        // loops statement
+        // initialization;condition;increment/decrement(updation)
+       for (int i=0;i<=10;i++){
+           System.out.println("num: "+i);
+       }
+
+
+
+
+    }
+}
